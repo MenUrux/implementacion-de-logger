a7db54e443ac3ebcb37a5ba0b6eee652a6e23bf6
menurux <jorgepino.utu@gmail.com>
2024-02-07 21:39:32 -0300

    fixed

--- src/routers/api/loggers.router.js
+++ src/routers/api/loggers.router.js
@@ -3,13 +3,17 @@ import { addLogger } from '../../utils/logger.js';
 
 const router = Router();
 
+
+/* const loggerMessage = `${req.method} - route: ${req.url} - ${new Date().toLocaleTimeString()}` */
+
 router.get('/', addLogger, (req, res) => {
-  req.logger.fatal('FATAL')
-  req.logger.error('ERROR')
-  req.logger.warning('WARNING')
-  req.logger.info('INFO')
-  req.logger.http('HTTP')
-  req.logger.debug('DEBUG')
+  const time = new Date().toLocaleTimeString();
+  req.logger.fatal(`${time} - ${req.method} - route: ${req.url}`)
+  req.logger.error(`${time} - ${req.method} - route: ${req.url}`)
+  req.logger.warning(`${time} - ${req.method} - route: ${req.url}`)
+  req.logger.info(`${time} - ${req.method} - route: ${req.url}`)
+  req.logger.http(`${time} - ${req.method} - route: ${req.url}`)
+  req.logger.debug(`${time} - ${req.method} - route: ${req.url}`)
   res.send('Prueba de logger realizada')
   console.log('------------')
 });
